af11422740eda112dd3aa1b01cd16bf56a3b7f56
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-05-18 00:11:36 +0300

    HWS-8 reducer,sort Users

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const stateCopy=[...state]
+            if (action.payload === 'up')return  stateCopy.sort((a, b) => a.name.localeCompare(b.name))
+            else if (action.payload === 'down') return stateCopy.sort((a, b) =>b.name.localeCompare(a.name))
+            else return stateCopy
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
             return state
